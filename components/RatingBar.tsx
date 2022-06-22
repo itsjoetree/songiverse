6d93a8a268b0d1e195f1ba1978e90165358eb689
itsjoetree <84997596+itsjoetree@users.noreply.github.com>
2022-06-22 19:53:02 -0400

    minor updates on index

--- components/RatingBar.tsx
+++ components/RatingBar.tsx
@@ -1,18 +1,20 @@
 import React from "react"
 import { Icon, StarFill, StarHalf } from "react-bootstrap-icons"
+import { useMediaQuery } from "react-responsive"
 import { Block } from "./styled/Containers.styled"
 
 const APP_COLOR = "#6495ED"
 
 const RatingBar = ({rating} : any) => {
+  const isTinyDevice = useMediaQuery({ query: '(max-width: 400px)' })
 
   const getStars = () : Icon[] => {
     const parsedRating = rating.toString()
     const nums = parsedRating.split('.')
 
-    const stars = new Array(parseInt(nums[0])).fill(<StarFill size={20} color={APP_COLOR} />);
+    const stars = new Array(parseInt(nums[0])).fill(<StarFill size={isTinyDevice ? 10 : 20} color={APP_COLOR} />);
 
-    if (nums.length > 1 && nums[1] >= 5) stars.push(<StarHalf size={20} color={APP_COLOR} />)
+    if (nums.length > 1 && nums[1] >= 5) stars.push(<StarHalf size={isTinyDevice ? 10 : 20} color={APP_COLOR} />)
 
     return stars
   }
--- components/styled/Containers.styled.tsx
+++ components/styled/Containers.styled.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 type FlexContainerProps = {
     direction?: string,
+    centerContent?: boolean,
 }
 
 export const FlexContainer = styled.div<FlexContainerProps>`
@@ -9,6 +10,8 @@ export const FlexContainer = styled.div<FlexContainerProps>`
     margin-right: 10px;
     display: flex;
     flex-direction: ${(props) => props.direction};
+
+    ${(props) =>  props.centerContent && "justify-content: center; align-items: center"}
 `
 
 export const Block = (props: any) => <div>{props.children}</div>
